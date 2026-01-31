4c15be14846602442551ae1a543c9b2168e1e3dc
Flo <florine.bienhuels@gmail.com>
2026-02-01 00:45:28 +0100

    last commits for today

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(5);
+        }
+    }
+}
